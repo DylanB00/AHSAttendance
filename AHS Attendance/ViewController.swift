24c1616e9bb43105964345bbd1383cdbd192aeed
Student <student@hs107-mac-mini.local>
2016-10-14 08:51:35 -0500

    Initial Commit

---
+++ AHS Attendance/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AHS Attendance
+//
+//  Created by Student on 10/14/16.
+//  Copyright © 2016 Dylan Bolger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
